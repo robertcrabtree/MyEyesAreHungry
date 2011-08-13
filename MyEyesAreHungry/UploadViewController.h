bd0ac8abd94bf909daefcd0dbf74472139a1cd00
Robert Crabtree <robert.crabtree@gmail.com>
2011-08-12 21:00:46 -0700

    UploadViewController: Add helper methods and selectors

--- MyEyesAreHungry/UploadViewController.h
+++ MyEyesAreHungry/UploadViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @class UploadArrays;
+@class TextCell;
 
 @interface UploadViewController : UITableViewController <UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource> {
 
@@ -21,5 +22,19 @@
 }
 
 @property (retain, nonatomic) UIImage *image;
+
+-(TextCell *) cellWithTag:(NSInteger)tag;
+-(TextCell *) cellInSection:(NSInteger)section AndRow:(NSInteger)row;
+-(BOOL)isValidData;
+-(void)getPostStrings:(NSInteger)row key:(NSString **)key val:(NSString **)val;
+-(BOOL)upload;
+-(BOOL)textFieldShouldEndEditing:(UITextField *)textField;
+-(BOOL)textFieldShouldReturn:(UITextField *)textField;
+-(BOOL)textFieldShouldBeginEditing:(UITextField *)textField;
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView;
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component;
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component;
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component;
+
 
 @end
